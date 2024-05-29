5db42491c3769c4666d183f2ab8641bfa889ba93
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2024-05-30 00:33:56 +0800

    方块强度

--- src/main/java/com/hexagram2021/redstoneclock/common/register/RCBlocks.java
+++ src/main/java/com/hexagram2021/redstoneclock/common/register/RCBlocks.java
@@ -21,13 +21,13 @@ public final class RCBlocks {
 	private static final DeferredRegister<Block> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
 	public static final RegistryObject<RedstoneClockBlock> REDSTONE_CLOCK = REGISTER.register("redstone_clock", () -> new RedstoneClockBlock(
-			BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops()
+			BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(1.5F)
 					.lightLevel(blockState -> blockState.getValue(RedstoneClockBlock.POWERED) ? 7 : 0)
 					.sound(SoundType.METAL).pushReaction(PushReaction.BLOCK)
 					.isValidSpawn((BlockState blockState, BlockGetter level, BlockPos blockPos, EntityType<?> entityType) -> false)
 	));
 	public static final RegistryObject<PulseDividerBlock> PULSE_DIVIDER = REGISTER.register("pulse_divider", () -> new PulseDividerBlock(
-			BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops()
+			BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(1.5F)
 					.sound(SoundType.METAL).pushReaction(PushReaction.BLOCK)
 					.isValidSpawn((BlockState blockState, BlockGetter level, BlockPos blockPos, EntityType<?> entityType) -> false)
 	));
